b1e143883cc856b529c90ba86ba911ece7465501
gleborgne <gleborgne@mcnext.com>
2015-04-07 23:18:33 +0200

    ES6

--- winjswebcomponents/js/customcontrol.js
+++ winjswebcomponents/js/customcontrol.js
@@ -3,13 +3,14 @@ var TestApp = TestApp || {};
 (function(){
 	'use strict';
 
-	var Control = function(element, options){
-		this.element = element || document.createElement('DIV');
-		this.element.winControl = this;
-		this.element.classList.add('a-custom-control');
-		this.element.innerHTML = 'this is content from a custom control : ' + options.text;
+	class Control {
+		 constructor(element, options){
+			this.element = element || document.createElement('DIV');
+			this.element.winControl = this;
+			this.element.classList.add('a-custom-control');
+			this.element.innerHTML = 'this is content from an ES6 custom control : ' + options.text;
+		}
 	}
-
 	TestApp.CustomControl = Control;
 
 	WinJSContrib.UI.WebComponents.register('my-customcontrol', Control, function (elt) {
