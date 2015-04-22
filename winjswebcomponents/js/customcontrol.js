772f9daa8feb31b6762f95d0986dde3d6f194d6d
gleborgne <gleborgne@mcnext.com>
2015-04-22 23:01:58 +0200

    tests

--- winjswebcomponents/js/customcontrol.js
+++ winjswebcomponents/js/customcontrol.js
@@ -3,13 +3,11 @@ var TestApp = TestApp || {};
 (function(){
 	'use strict';
 
-	class Control {
-		 constructor(element, options){
+	function Control(element, options) {		 
 			this.element = element || document.createElement('DIV');
 			this.element.winControl = this;
 			this.element.classList.add('a-custom-control');
-			this.element.innerHTML = 'this is content from an ES6 custom control : ' + options.text;
-		}
+			this.element.innerHTML = 'this is content from an ES6 custom control : ' + options.text;		
 	}
 	TestApp.CustomControl = Control;
 
