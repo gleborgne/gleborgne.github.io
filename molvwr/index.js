4f925c45303219b34f659f7373f28bf6c904f082
gleborgne <gleborgne@mcnext.com>
2015-10-08 01:12:29 +0200

    update molvwr

--- molvwr/index.js
+++ molvwr/index.js
@@ -2,5 +2,5 @@
 	'use strict';
 	var root = document.getElementById("molvwr-container");
 	var viewer = new Molvwr.Viewer(root);
-	viewer.loadContentFromUrl("molsamples/xyz/caffeine.txt", "xyz");
+	viewer.loadContentFromUrl("molsamples/xyz/dna.txt", "xyz");
 })();
