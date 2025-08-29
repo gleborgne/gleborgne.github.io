102de3e22781b186a108ef160301d032d2406ed7
Guillaume LEBORGNE <gleborgne@inwink.com>
2025-08-29 15:03:57 +0200

    website setup

--- winjswebcomponents/js/default.js
+++
@@ -1,7 +0,0 @@
-AppVersion = '1.1';
-WinJS.Utilities.ready(function(){
-	WinJS.UI.processAll(document.body).then(function(){
-		console.log('DONE !!!');
-		WinJS.Navigation.navigate('/winjswebcomponents/pages/home/home.html?v=' + AppVersion, { version: '1' });
-	});
-});
